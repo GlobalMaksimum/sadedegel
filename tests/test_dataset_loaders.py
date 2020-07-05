6ee3c29bc433e0f64e0af93b329837d88ee0cb01
Husnu Sensoy <husnu.sensoy@globalmaksimum.com>
2020-07-06 01:00:22 +0300

    Rename+Enrich test scenario for dataset loaders.

---
+++ tests/test_dataset_loaders.py
@@ -0,0 +1,31 @@
+from .context import load_raw_corpus, load_sentence_corpus
+
+
+def test_load_raw_iter():
+    doc_iter = load_raw_corpus(return_iter=True)
+
+    assert sum(1 for _ in doc_iter) == 100
+
+
+def test_load_raw_list():
+    doc_list = load_raw_corpus(return_iter=False)
+
+    assert len(doc_list) == 100
+
+
+def test_load_sent_iter():
+    doc_iter = load_sentence_corpus(return_iter=True)
+
+    assert sum(1 for _ in doc_iter) == 100
+
+
+def test_load_sent_list():
+    doc_list = load_sentence_corpus(return_iter=False)
+
+    assert len(doc_list) == 100
+
+
+def test_proper_dictionary():
+    doc_list = load_sentence_corpus(return_iter=False)
+
+    assert all((('sentences' in doc) for doc in doc_list))
