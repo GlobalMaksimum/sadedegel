5195a5321c3cd07277069f03e7e21c2ae7eb2cf3
Husnu Sensoy <husnu.sensoy@globalmaksimum.com>
2020-07-24 22:22:28 +0300

    Fix test case broken by Sentence class implementation (sent object is no longer in List[str] type)

--- tests/test_sent_tokenizer.py
+++ tests/test_sent_tokenizer.py
@@ -42,9 +42,11 @@ def test_ml_sent_tokenizer_edge_cases():
 
     doc = Doc(doc_str)
 
-    assert doc.sents == ['Hz. İsa M.Ö. 0. yılda doğdu.', 'Doç. Dr. Mehmet Bey kanserin ilacını buldu!!!', 'Aşk…',
-                         "14 Şubat'ta olmasın…", "Kocatepe Mah.de, Güneş Sok.ta gerçekleşen olay herkesi şaşırttı!",
-                         "Alb. Gen. Mehmet Bey kendi evine saldırı düzenledi...",
-                         "K.K.T.C'de eşek nüfusu kontrol dışında!",
-                         "Av. İst. Prof. Mehmet Bey Tahtalıköy'e uğradı.",
-                         "123. Türkiye E-Sports turnuvası İstanbul'da gerçekleşti."]
+    assert [sent.text for sent in doc.sents] == ["Hz. İsa M.Ö. 0. yılda doğdu.",
+                                                 'Doç. Dr. Mehmet Bey kanserin ilacını buldu!!!', 'Aşk…',
+                                                 "14 Şubat'ta olmasın…",
+                                                 "Kocatepe Mah.de, Güneş Sok.ta gerçekleşen olay herkesi şaşırttı!",
+                                                 "Alb. Gen. Mehmet Bey kendi evine saldırı düzenledi...",
+                                                 "K.K.T.C'de eşek nüfusu kontrol dışında!",
+                                                 "Av. İst. Prof. Mehmet Bey Tahtalıköy'e uğradı.",
+                                                 "123. Türkiye E-Sports turnuvası İstanbul'da gerçekleşti."]
