6c056726c867ae230f4206919663f77f3c8121dd
Husnu Sensoy <husnu.sensoy@globalmaksimum.com>
2020-07-28 22:23:46 +0300

    Better sadedegel.server capabilities and API standard [resolves: #14, #22]
       * / not redirects to sadedegel.ai
       * Summarizer routes now takes
           * **doc**: Text to summarize
           * **wpm**: Number of words that can be read per minute by the
             user.
           * **duration**: Total user reading time (unit defined by **unit**)
       * Summarizer responds with summary and summary statistics.
       * Rouge1 Summarizer is added as the first non-baseline summarizer.
       * `/api/info` returns API version metadata.

--- tests/server/test_basics.py
+++ tests/server/test_basics.py
@@ -7,24 +7,26 @@ client = TestClient(app)
 
 
 def test_random_summarizer_req():
-    response = client.post("/sadedegel/random", json=dict(
-        text="Kapıyı aç Veysel Efendi! Mahmut Hoca'nın emriyle Uganda Cumhurbaşkanı'nı karşılamaya gidiyoruz."))
+    response = client.post("/api/summarizer/random", json=dict(
+        doc="Kapıyı aç Veysel Efendi! Mahmut Hoca'nın emriyle Uganda Cumhurbaşkanı'nı karşılamaya gidiyoruz.", wpm=1,
+        duration=1))
     assert response.status_code == 200
     assert 'sentences' in response.json()
 
 
 def test_firstk_summarizer_req():
-    response = client.post("/sadedegel/firstk", json=dict(
-        text="Kapıyı aç Veysel Efendi! Mahmut Hoca'nın emriyle Uganda Cumhurbaşkanı'nı karşılamaya gidiyoruz."))
+    response = client.post("/api/summarizer/random", json=dict(
+        doc="Kapıyı aç Veysel Efendi! Mahmut Hoca'nın emriyle Uganda Cumhurbaşkanı'nı karşılamaya gidiyoruz.", wpm=1,
+        duration=1))
     assert response.status_code == 200
     assert 'sentences' in response.json()
 
 
-testdata = [('/sadedegel/random', 'https://www.hurriyet.com.tr', 'POST'),
-            ('/sadedegel', 'https://www.hurriyet.com.tr', 'GET'),
-            ('/sadedegel/random', 'http://0.0.0.0:8000/sadedegel', 'POST'),
-            ('/sadedegel/random', 'https://www.milliyet.com.tr', 'GET'),
-            ('/sadedegel/random', 'https://www.sozcu.com.tr', 'GET')]
+testdata = [('/api/summarizer/random', 'https://www.hurriyet.com.tr', 'POST'),
+            ('/', 'https://www.hurriyet.com.tr', 'GET'),
+            ('/api/summarizer/random', 'http://0.0.0.0:8000/sadedegel', 'POST'),
+            ('/api/summarizer/random', 'https://www.milliyet.com.tr', 'GET'),
+            ('/api/summarizer/random', 'https://www.sozcu.com.tr', 'GET')]
 
 
 @pytest.mark.parametrize("url, origin, method", testdata)
