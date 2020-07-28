6c3c0cbf84aab188d56e908e8e7bbf06bbd86fb9
Husnu Sensoy <husnu.sensoy@globalmaksimum.com>
2020-07-28 22:32:31 +0300

    Release version

--- sadedegel/about.py
+++ sadedegel/about.py
@@ -1,4 +1,4 @@
 __title__ = "sadedegel"  # pragma: no cover
-__version__ = "0.4.2"  # pragma: no cover
+__version__ = "0.5"  # pragma: no cover
 __release__ = True  # pragma: no cover
 __download_url__ = "https://github.com/globalmaksimum/sadedegel/releases"  # pragma: no cover
