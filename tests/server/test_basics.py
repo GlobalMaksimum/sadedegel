197eae2b84d1ffa3eb1a8594b07233e387d08d64
Husnu Sensoy <husnu.sensoy@globalmaksimum.com>
2020-07-29 15:17:39 +0300

    Issue  #31 [resolves: #31]     Add several test cases to reproduce issue but couldn't succeed.

--- tests/server/test_basics.py
+++ tests/server/test_basics.py
@@ -22,6 +22,43 @@ def test_firstk_summarizer_req():
     assert 'sentences' in response.json()
 
 
+def test_random_summarizer_wpm0():
+    response = client.post("/api/summarizer/random", json=dict(
+        doc="Kapıyı aç Veysel Efendi! Mahmut Hoca'nın emriyle Uganda Cumhurbaşkanı'nı karşılamaya gidiyoruz.", wpm=0,
+        duration=1))
+
+    assert response.status_code == 200
+    assert 'sentences' in response.json()
+
+
+def test_random_summarizer_duration0():
+    response = client.post("/api/summarizer/random", json=dict(
+        doc="Kapıyı aç Veysel Efendi! Mahmut Hoca'nın emriyle Uganda Cumhurbaşkanı'nı karşılamaya gidiyoruz.", wpm=150,
+        duration=0))
+
+    assert response.status_code == 200
+    assert 'sentences' in response.json()
+
+
+def test_random_summarizer_wpm0_duration0():
+    response = client.post("/api/summarizer/random", json=dict(
+        doc="Kapıyı aç Veysel Efendi! Mahmut Hoca'nın emriyle Uganda Cumhurbaşkanı'nı karşılamaya gidiyoruz.", wpm=0,
+        duration=0))
+
+    assert response.status_code == 200
+    assert 'sentences' in response.json()
+
+
+def test_random_summarizer_nosentence():
+    response = client.post("/api/summarizer/random", json=dict(
+        doc="Kapıyı aç Veysel Efendi", wpm=0,
+        duration=0))
+
+    assert response.status_code == 200
+    assert 'sentences' in response.json()
+    assert len(response.json()['sentences']) == 0
+
+
 testdata = [('/api/summarizer/random', 'https://www.hurriyet.com.tr', 'POST'),
             ('/', 'https://www.hurriyet.com.tr', 'GET'),
             ('/api/summarizer/random', 'http://0.0.0.0:8000/sadedegel', 'POST'),
