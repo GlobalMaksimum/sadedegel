46b40525600c32d491171930dbfceff4fb15d138
Husnu Sensoy <husnu.sensoy@globalmaksimum.com>
2020-07-31 15:27:42 +0300

    Replace logger with warning package for safe loaders

--- tests/datasets/context.py
+++ tests/datasets/context.py
@@ -5,3 +5,4 @@ sys.path.insert(0, (Path(__file__) / '..' / '..').absolute())
 
 from sadedegel.dataset import load_raw_corpus, load_sentence_corpus,load_annotated_corpus  # noqa # pylint: disable=unused-import, wrong-import-position
 from sadedegel.dataset.extended import load_extended_metadata, load_extended_sents_corpus, load_extended_raw_corpus  # noqa # pylint: disable=unused-import, wrong-import-position
+from sadedegel.dataset import util # noqa # pylint: disable=unused-import, wrong-import-position
