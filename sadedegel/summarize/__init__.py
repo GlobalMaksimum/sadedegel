efb991e15173484c5913e3f8d3301412760cf82f
Oscar <askar.bozcan@metu.edu.tr>
2020-08-07 22:16:46 +0300

    Added LexRank summarizer

--- sadedegel/summarize/__init__.py
+++ sadedegel/summarize/__init__.py
@@ -1,2 +1,3 @@
 from .baseline import RandomSummarizer, PositionSummarizer, LengthSummarizer, BandSummarizer  # noqa: F401
 from .rouge import Rouge1Summarizer
+from .lexrank_summ import LexRankSummarizer
--- sadedegel/summarize/lexrank_summ.py
+++ sadedegel/summarize/lexrank_summ.py
@@ -1,4 +1,33 @@
+import numpy as np
 from lexrank import LexRank
-from ..dataset import load_stopwords
+from ..dataset import load_stopwords, load_raw_corpus
+from ._base import ExtractiveSummarizer
 
-print(load_stopwords())
+class LexRankSummarizer(ExtractiveSummarizer):
+    """
+        Unsupervised summarizer which just like Rouge1 summarizer
+        uses sentence's similarity to other sentences.
+        Github: https://github.com/crabcamp/lexrank/
+    """
+
+    def __init__(self, normalize=True):
+        self.normalize = normalize
+
+        stopwords = set(load_stopwords())
+        corpus = load_raw_corpus(return_iter=False)
+        self.lxr = LexRank(corpus, stopwords=stopwords)
+
+
+    def predict(self, sentences):
+        sentences = [str(s) for s in sentences]
+        scores = self.lxr.rank_sentences(
+            sentences,
+            threshold=None,
+            fast_power_method=False,
+        )
+
+        scores = np.array(scores)
+        if self.normalize:
+            return scores / scores.sum()
+        else:
+            return scores
