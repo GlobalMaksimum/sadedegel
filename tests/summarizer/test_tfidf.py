8943512036c938277759a035b872450b65ac8e0d
Husnu Sensoy <husnu.sensoy@globalmaksimum.com>
2020-09-29 00:51:46 +0300

    Fix test cases:     Follow linter exception rules noqa and disable=unused-import for new summarizer
        We NEVER build Sentences class directly calling Sentence class constructor which is a caveat in current library design.
          We should prevent this.

--- tests/summarizer/test_tfidf.py
+++ tests/summarizer/test_tfidf.py
@@ -1,14 +1,8 @@
-from sadedegel.tokenize import Sentences
 from .context import TFIDFSummarizer, Doc
 import numpy as np
 import pytest
 
 
-@pytest.mark.parametrize("docs", [["Onu hiç sevmedim", "Bu iş çok zor"]])
-@pytest.mark.parametrize("inp_doc", [[Sentences(0, "Bu renk kötü",
-                                                Doc("Bu renk kötü. O araç güzel")),
-                                      Sentences(1, "O araç güzel",
-                                                Doc("Bu renk kötü. O araç güzel"))]])
-@pytest.mark.parametrize("result", [np.array([0.23104906, 0.34657359])])
-def test_tfidf(docs, inp_doc, result):
-    assert TFIDFSummarizer(docs).predict(inp_doc) == pytest.approx(result)
+def test_tfidf():
+    d = Doc("Onu hiç sevmedim. Bu iş çok zor.")
+    assert TFIDFSummarizer().predict(d.sents) == pytest.approx(np.array([0.63611803, 0.36388197]))
