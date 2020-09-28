30fd9e86d33d03922223a4574131e1830ab287d4
Husnu Sensoy <husnu.sensoy@globalmaksimum.com>
2020-09-29 00:46:55 +0300

    Fixes on tfidf summarizer:     Add tags for the summarizer
        Summarizers just summarize. They shouldn't build a vocabulary in constructor.
        Vocabulary construction is performed by bblock/__main__ build_vocabulary command.
        _predict function of the class simply calls tfidf() function on Sentences instances of Doc and returns
          element-wise sum of tfidf vector.

--- sadedegel/summarize/tf_idf.py
+++ sadedegel/summarize/tf_idf.py
@@ -8,36 +8,10 @@ from ._base import ExtractiveSummarizer
 
 
 class TFIDFSummarizer(ExtractiveSummarizer):
-    def __init__(self, all_docs: List[str], normalize=False):
+    tags = ExtractiveSummarizer.tags + ['ml']
+
+    def __init__(self, normalize=True):
         super().__init__(normalize)
-        all_words_dict = defaultdict(lambda: 0)
-        doc_count = 0
-        for doc_str in all_docs:
-            doc = Doc(doc_str)
-            unique_words = set()
-            for sent in doc.sents:
-                for token in sent.tokens:
-                    unique_words.add(token)
-            for token in unique_words:
-                all_words_dict[token] += 1
-            doc_count += 1
-        self.all_words_dict = all_words_dict
-        self.doc_count = doc_count
 
     def _predict(self, sents: List[Sentences]):
-        doc_word_dict = defaultdict(lambda: 0)
-        doc_word_count = 0
-        for sent in sents:
-            for token in sent.tokens:
-                doc_word_dict[token] += 1
-                doc_word_count += 1
-        scores = [None] * len(sents)
-        i = 0
-        for sent in sents:
-            scores[i] = []
-            for token in sent.tokens:
-                scores[i].append((doc_word_dict[token] / doc_word_count) * log(
-                                 (self.doc_count / (1 + self.all_words_dict[token]))))
-            scores[i] = sum(scores[i])
-            i += 1
-        return np.array(scores)
+        return np.array([sent.tfidf().sum() for sent in sents])
