b851e4b382fb85e1e55528fee347f148cca17e1e
Husnu Sensoy <husnu.sensoy@globalmaksimum.com>
2020-10-02 22:44:57 +0300

    Rename & Remove redundant tests

--- tests/test_token_attr.py
+++ tests/test_token_attr.py
@@ -1,34 +1,22 @@
 import pytest
-from .context import Token, Doc
-
+from .context import Token
 
 famous_quote = "Merhaba dünya. 36 ışıkyılı uzaktan geldik."
 
 
-def test_token():
-
-    d = Doc(famous_quote)
-    s = d.sents[0]
-    t = s.tokens[0]
-
-    assert isinstance(t) == str
-
-
 def test_is_digit():
-
-    t = Token(0, '123', 10, 100)
+    t = Token('123')
     assert t.is_digit
 
 
 def test_is_punct():
-
-    t = Token(0, '!', 10, 100)
+    t = Token('!')
     assert t.is_punct
 
 
 @pytest.mark.parametrize("word, shape", [('1969', 'dddd'),
                                          ('Hello', 'Xxxxx')])
 def test_shape(word, shape):
-    t = Token(0, word, 10, 100)
+    t = Token(word)
 
     assert t.shape == shape
