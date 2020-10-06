415690f3c3a356077c5a489f48353e3a30dbfc84
dafajon <dorukhan.afacan@globalmaksimum.com>
2020-10-06 17:38:34 +0300

    Add tests for new tf functions and related config option.
    **`test_buildingblocks.py`:**
    - Add other `tf_type` options to `test_tf_idf_embedding_generation`.
    - Add other `tf_type` options to `test_tfidf_compare_doc_and_sent`.
    
    **`test_config.py`
    - Add test for `tf_config_print`
    - Add test for setting `tf` config with an invalid value.
    
    **`test_tfidf.py`:**
    - Test `TFIDFSummarizer` output with alternative `tf` functions that are implemented in `Sentence`.

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,4 +1,5 @@
-from .context import get_all_configs, describe_config
+from pytest import raises
+from .context import get_all_configs, describe_config, set_config
 
 
 def test_all_configs():
@@ -15,3 +16,18 @@ def test_describe_config_print(capsys):
 
     assert 'Change the default word tokenizer used by sadedegel' in captured.out or \
            'Change the default word tokenizer used by sadedegel' in captured.err
+
+
+def test_describe_tf_config_print(capsys):
+    describe_config('tf', True)
+    captured = capsys.readouterr()
+
+    assert 'Change default tf function used by sadedegel' \
+           '\n\nValid values are\nbinary\nraw\nfreq\nlog_norm\ndouble_norm\n' in captured.out or \
+           'Change default tf function used by sadedegel' \
+           '\n\nValid values are\nbinary\nraw\nfreq\nlog_norm\ndouble_norm\n' in captured.err
+
+
+def test_tf_setting():
+    with raises(Exception, match=r".*is not a valid value.*"):
+        set_config('tf', 'double_norm_k')
