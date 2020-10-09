4d6ec1a5c6b8855d008cf2e2fd87f7fef2df7be4
dafajon <dorukhan.afacan@globalmaksimum.com>
2020-10-09 12:00:09 +0300

    Update package data with file hierarchy of vocabulary.json files.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     package_data={
         'sadedegel.dataset': ['raw/*.txt', 'sents/*.json', 'annotated/*.json'],
         'sadedegel.ml': ['model/sbd.pickle'],
-        'sadedegel.bblock': ['data/vocabulary.json']
+        'sadedegel.bblock': ['data/*/vocabulary.json']
     },
     # Needed for dependencies
     install_requires=install_requires,
