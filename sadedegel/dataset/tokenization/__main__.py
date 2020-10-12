9532f868e5198ff658ca820aa7c9546ce7f35bf5
dafajon <dorukhan.afacan@globalmaksimum.com>
2020-10-13 00:01:00 +0300

    Build raw and tokenized json files from tokenization dataset.
    - Dataset ready for `load_corpus` function.
    - `dataset.tokenization.raw` and `dataset.tokenization.tokenized` will have respective `load_corpus` functions.
    - Return Iter[dict], List[dict]. Each element having {'doc_id': int, 'category': str ,'text':str} for raw and {'doc_id': int, 'category': str ,'tokens':List[str]} for tokenized.

--- sadedegel/dataset/tokenization/__main__.py
+++ sadedegel/dataset/tokenization/__main__.py
@@ -3,6 +3,9 @@ import os.path
 from loguru import logger
 from pathlib import Path
 import boto3
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+import json
 from smart_open import open
 import tarfile
 
@@ -56,6 +59,59 @@ def download(access_key, secret_key, data_home, data_version):
     d = check_and_display('~/.sadedegel_data')
     click.secho("Checking download...")
     click.secho("Dataset stats: " + click.style(f"{d}", fg="yellow"), color='white')
+
+
+@cli.command()
+@click.option("--data_home", '-d', help="Data home directory", default="~/.sadedegel_data")
+@click.option('--data-version', "-v", help="Dataset version", default="v2")
+def prepare(data_home, data_version):
+
+    __txts__ = ["Raw_Text_80M_Portion_of_TimeLine.txt",
+                "Tokenized_Text_80M_Portion_of_TimeLine.txt"]
+
+    click.secho("Reading "+click.style("Raw .txt file.", fg='blue'))
+    with open(str(Path(os.path.expanduser(data_home)) / 'tokenization' / data_version / "raw" / __txts__[0]), 'r') as f:
+        raw = ''.join(f.readlines())
+    f.close()
+
+    click.secho("Parsing "+click.style("Raw data.", fg='blue'))
+    soup = BeautifulSoup(raw, 'html.parser')
+    documents = []
+    for link in tqdm(soup.find_all('text')):
+        d = {
+            'index': int(link.get('document').split('_')[-1]) - 1000,
+            'doc_id': link.get('document'),
+            'category': link.get('category'),
+            'text': link.get_text()[1:-1]
+        }
+        documents.append(d)
+
+    click.secho("Saving " + click.style("Raw .json file.", fg='blue'))
+    with open(str(Path(os.path.expanduser(data_home)) / 'tokenization' / data_version / "raw" / "Raw.json"), 'w') as j:
+        json.dump(documents, j)
+
+    click.secho("Reading " + click.style("Tokenized .txt file.", fg='blue'))
+    with open(str(Path(os.path.expanduser(data_home)) / 'tokenization' / data_version / "tokenized" / __txts__[1]),
+              'r') as f:
+        tok = ''.join(f.readlines())
+    f.close()
+
+    click.secho("Parsing " + click.style("Tokenized data.", fg='blue'))
+    soup2 = BeautifulSoup(tok, 'html.parser')
+    tokens = []
+    for link in tqdm(soup2.find_all('text')):
+        d = {
+            'index': int(link.get('document').split('_')[-1]) - 1000,
+            'doc_id': link.get('document'),
+            'category': link.get('category'),
+            'tokens': link.get_text().split('\n')[1:-1]
+        }
+        tokens.append(d)
+
+    click.secho("Saving " + click.style("Tokenized .json file.", fg='blue'))
+    with open(str(Path(os.path.expanduser(data_home)) / 'tokenization' / data_version / "tokenized" / "Tokenized.json"),
+              'w') as j:
+        json.dump(documents, j)
 
 
 if __name__ == "__main__":
