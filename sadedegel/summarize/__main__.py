e504778ca3085137f18a4450d64f40adc80e5a66
dafajon <dorukhan.afacan@globalmaksimum.com>
2021-01-25 17:59:23 +0300

    Incorporate get_bm25 into BM25Summarizer

--- sadedegel/summarize/__main__.py
+++ sadedegel/summarize/__main__.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 from math import ceil
 from typing import List
 from loguru import logger
+from itertools import product
 
 import click
 import time
@@ -175,19 +176,51 @@ def evaluate(table_format, tag, debug):
     # BM25 Evaluation
     name, summarizer = "BM25 Summarizer", BM25Summarizer()
 
+    stopword = [True, False]
+    punct = [True, False]
+    suffix = [True, False]
+    lower = [True, False]
+
+    bm25_word_settings = product(stopword, punct, suffix, lower)
+
     if any(_tag in summarizer for _tag in tag):
         for k1 in np.linspace(1.2, 2.0, 5):
             for b in np.linspace(0.5, 0.8, 5):
-                summarizer = BM25Summarizer(k1=k1, b=b)
-                for tf in TF_METHOD_VALUES:
-                    for idf in IDF_METHOD_VALUES:
-                        if tf == "double_norm":
-                            for k in [0.25, 0.5, 0.75]:
-                                with config_context(tokenizer="bert", tf__method=tf, idf__method=idf,
-                                                    tf__double_norm_k=k) as Doc2:
+                for drop_stopwords, drop_punct, drop_suffix, lowercase in bm25_word_settings:
+                    summarizer = BM25Summarizer(k1=k1, b=b,
+                                                drop_stopwords=drop_stopwords, drop_punct=drop_punct,
+                                                drop_suffix=drop_suffix, lowercase=lowercase)
+                    for tf in TF_METHOD_VALUES:
+                        for idf in IDF_METHOD_VALUES:
+                            if tf == "double_norm":
+                                for k in [0.25, 0.5, 0.75]:
+                                    with config_context(tokenizer="bert", tf__method=tf, idf__method=idf,
+                                                        tf__double_norm_k=k) as Doc2:
+                                        t0 = time.time()
+                                        table_key = f"{name} (tf={tf}, double_norm_k={k}, idf={idf}, k1={k1}, b={b}, " \
+                                                    f"drop_stopwords={drop_stopwords}, drop_punct={drop_punct}, " \
+                                                    f"drop_suffix={drop_suffix}, lowercase={lowercase}, tokenizer=bert)"
+                                        click.echo(click.style(f"    {table_key}", fg="magenta"), nl=False)
+
+                                        docs = [Doc2.from_sentences(doc['sentences']) for doc in
+                                                anno]
+
+                                        for i, (y_true, d) in enumerate(zip(relevance, docs)):
+                                            dot_progress(i, len(relevance), t0)
+
+                                            y_pred = [summarizer.predict(d)]
+
+                                            score_10 = ndcg_score(y_true, y_pred, k=ceil(len(d) * 0.1))
+                                            score_50 = ndcg_score(y_true, y_pred, k=ceil(len(d) * 0.5))
+                                            score_80 = ndcg_score(y_true, y_pred, k=ceil(len(d) * 0.8))
+
+                                            scores[table_key].append((score_10, score_50, score_80))
+                            else:
+                                with config_context(tokenizer="bert", tf__method=tf, idf__method=idf) as Doc2:
                                     t0 = time.time()
-                                    table_key = f"{name} (tf={tf}, double_norm_k={k}, idf={idf}, k1={k1}, b={b}, " \
-                                                f"tokenizer=bert)"
+                                    table_key = f"{name} (tf={tf}, idf={idf}, k1={k1}, b={b}," \
+                                                f"drop_stopwords={drop_stopwords}, drop_punct={drop_punct}, " \
+                                                f"drop_suffix={drop_suffix}, lowercase={lowercase}, tokenizer=bert)"
                                     click.echo(click.style(f"    {table_key}", fg="magenta"), nl=False)
 
                                     docs = [Doc2.from_sentences(doc['sentences']) for doc in
@@ -203,25 +236,6 @@ def evaluate(table_format, tag, debug):
                                         score_80 = ndcg_score(y_true, y_pred, k=ceil(len(d) * 0.8))
 
                                         scores[table_key].append((score_10, score_50, score_80))
-                        else:
-                            with config_context(tokenizer="bert", tf__method=tf, idf__method=idf) as Doc2:
-                                t0 = time.time()
-                                table_key = f"{name} (tf={tf}, idf={idf}, k1={k1}, b={b}, tokenizer=bert)"
-                                click.echo(click.style(f"    {table_key}", fg="magenta"), nl=False)
-
-                                docs = [Doc2.from_sentences(doc['sentences']) for doc in
-                                        anno]
-
-                                for i, (y_true, d) in enumerate(zip(relevance, docs)):
-                                    dot_progress(i, len(relevance), t0)
-
-                                    y_pred = [summarizer.predict(d)]
-
-                                    score_10 = ndcg_score(y_true, y_pred, k=ceil(len(d) * 0.1))
-                                    score_50 = ndcg_score(y_true, y_pred, k=ceil(len(d) * 0.5))
-                                    score_80 = ndcg_score(y_true, y_pred, k=ceil(len(d) * 0.8))
-
-                                    scores[table_key].append((score_10, score_50, score_80))
 
     table = [[algo, np.array([s[0] for s in scores]).mean(), np.array([s[1] for s in scores]).mean(),
               np.array([s[2] for s in scores]).mean()] for
--- sadedegel/summarize/bm25.py
+++ sadedegel/summarize/bm25.py
@@ -17,11 +17,36 @@ class BM25Summarizer(ExtractiveSummarizer):
     """
     tags = ExtractiveSummarizer.tags + ['self-supervised', 'ml', 'info-retrieval']
 
-    def __init__(self, k1=1.25, b=0.75, normalize=True):
+    def __init__(self, k1=1.25, b=0.75,
+                 tf_method=None, idf_method=None,
+                 drop_stopwords=False, drop_suffix=False,
+                 drop_punct=False, lowercase=False,
+                 config=None, normalize=True):
         super().__init__(normalize)
 
         self.k1 = k1
         self.b = b
 
-    def _predict(self, sentences: List[Sentences]):
-        return np.array([sent.bm25(k1=self.k1, b=self.b) for sent in sentences])
+        self.drop_stopwords = drop_stopwords
+        self.drop_suffix = drop_suffix
+        self.drop_punct = drop_punct
+        self.lowercase = lowercase
+
+        self.config = config
+        self.tf_method = tf_method
+        self.idf_method = idf_method
+
+    def _predict(self, sents: List[Sentences]):
+        if self.config is None:
+            self.config = sents[0].document.builder.config
+        if self.tf_method is None:
+            self.tf_method = self.config['tf']['method']
+        if self.idf_method is None:
+            self.idf_method = self.config['idf']['method']
+
+        return np.array([sent.get_bm25(tf_method=self.tf_method,
+                                       idf_method=self.idf_method,
+                                       drop_stopwords=self.drop_stopwords,
+                                       drop_suffix=self.drop_suffix,
+                                       drop_punct=self.drop_punct,
+                                       k1=self.k1, b=self.b).sum() for sent in sents])
