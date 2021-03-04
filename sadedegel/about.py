57738ef7e1945d7f7bfc82586ae8c39088783aa6
Husnu Sensoy <husnu.sensoy@globalmaksimum.com>
2021-03-05 02:38:58 +0300

    Update average sentence and document length configurations after sbd bug fix.

--- sadedegel/about.py
+++ sadedegel/about.py
@@ -1,5 +1,5 @@
 __title__ = "sadedegel"  # pragma: no cover
-__version__ = "0.18.2"  # pragma: no cover
+__version__ = "0.18.3"  # pragma: no cover
 __release__ = True  # pragma: no cover
 __download_url__ = "https://github.com/globalmaksimum/sadedegel/releases"  # pragma: no cover
 __herokuapp_url__ = "https://sadedegel.herokuapp.com"  # pragma: no cover
