58f054d6b68610d12be0d54aa9073a32da91f116
dafajon <dorukhan.afacan@globalmaksimum.com>
2021-03-07 16:24:02 +0300

    Implement telco_sentiment dataset CLI and loaders

--- tests/datasets/context.py
+++ tests/datasets/context.py
@@ -7,6 +7,8 @@ from sadedegel.dataset import load_raw_corpus, load_sentence_corpus,load_annotat
 from sadedegel.dataset.extended import load_extended_metadata, load_extended_sents_corpus, load_extended_raw_corpus  # noqa # pylint: disable=unused-import, wrong-import-position
 from sadedegel.dataset.tscorpus import load_tokenization_raw,load_tokenization_tokenized, check_and_display, CORPUS_SIZE # noqa # pylint: disable=unused-import, wrong-import-position
 from sadedegel.dataset.tweet_sentiment import load_tweet_sentiment_train, CLASS_VALUES # noqa # pylint: disable=unused-import, wrong-import-position
+from sadedegel.dataset.telco_sentiment import load_telco_sentiment_train, load_telco_sentiment_test, load_telco_sentiment_target # noqa # pylint: disable=unused-import, wrong-import-position
+from sadedegel.dataset.telco_sentiment import CLASS_VALUES as TELCO_CLASS_VALUES # noqa # pylint: disable=unused-import, wrong-import-position
 from sadedegel.bblock.cli.__main__ import tok_eval # noqa # pylint: disable=unused-import, wrong-import-position
 from sadedegel.dataset import util # noqa # pylint: disable=unused-import, wrong-import-position
 from sadedegel.dataset import file_paths, CorpusTypeEnum # noqa # pylint: disable=unused-import, wrong-import-position
---
+++ tests/datasets/test_telco_sentiment_corpus.py
@@ -0,0 +1,4 @@
+
+
+from .context import load_telco_sentiment_train, load_telco_sentiment_test, load_telco_sentiment_target
+from .context import TELCO_CLASS_VALUES
