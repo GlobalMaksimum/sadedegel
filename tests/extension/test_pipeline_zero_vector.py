12967ac4e5655d422dca13bdc2e598e707a0155a
Husnu Sensoy <husnu.sensoy@globalmaksimum.com>
2021-03-23 22:09:01 +0300

    Update test cases after Text2Doc transformer.

--- tests/extension/test_pipeline_zero_vector.py
+++ tests/extension/test_pipeline_zero_vector.py
@@ -11,7 +11,7 @@ from sklearn.linear_model import SGDClassifier
 
 from rich.progress import track
 
-from .context import OnlinePipeline, TfidfVectorizer, load_extended_raw_corpus
+from .context import OnlinePipeline, TfidfVectorizer, load_extended_raw_corpus, Text2Doc, tokenizer_context
 
 BATCH_SIZE = 10
 
@@ -19,7 +19,8 @@ BATCH_SIZE = 10
 @pytest.mark.skipif('not Path(expanduser("~/.sadedegel_data")).exists()')
 @pytest.mark.parametrize('tf_type, text', product(['freq', 'double_norm'], ['ıyi', '*******', '😗😗😗😗😗😗😗😗😗😗']))
 def test_tfidf_vectorizer_smoothing(tf_type, text):
-    pipeline = OnlinePipeline([('tfidf', TfidfVectorizer(tf_method=tf_type, idf_method='smooth')),
+    pipeline = OnlinePipeline([('t2d', Text2Doc()),
+                               ('tfidf', TfidfVectorizer(tf_method=tf_type, idf_method='smooth')),
                                ('model', SGDClassifier())])
 
     X1 = islice(load_extended_raw_corpus(), 101)
@@ -41,17 +42,23 @@ def test_tfidf_vectorizer_smoothing(tf_type, text):
 @pytest.mark.parametrize('tf_type, text', product(['freq', 'double_norm'], ['ıyi', '*******']))
 def test_zero_vector_edge_cases(tf_type, text):
     vectorizer = TfidfVectorizer(tf_method=tf_type, idf_method='smooth')
-    assert vectorizer.fit_transform([text]).sum() == 0
+
+    with tokenizer_context("icu") as Doc:
+        assert vectorizer.fit_transform([Doc(text)]).sum() == 0
 
 
 @pytest.mark.skipif('pkgutil.find_loader("transformers") is None')
 @pytest.mark.parametrize('tf_type, text', product(['freq', 'double_norm'], ['😗😗😗😗😗😗😗😗😗😗']))
 def test_zero_vector_edge_case_emoji(tf_type, text):
-    vectorizer = TfidfVectorizer(tokenizer="bert", tf_method=tf_type, idf_method='smooth')
-    assert vectorizer.fit_transform([text]).sum() == approx(4.479194)
+    vectorizer = TfidfVectorizer(tf_method=tf_type, idf_method='smooth')
+
+    with tokenizer_context("bert") as Doc:
+        assert vectorizer.fit_transform([Doc(text)]).sum() == approx(4.479194)
 
 
 @pytest.mark.parametrize('tf_type, text', product(['freq', 'double_norm'], ['😗😗😗😗😗😗😗😗😗😗']))
 def test_zero_vector_edge_case_emoji_icu(tf_type, text):
     vectorizer = TfidfVectorizer(tf_method=tf_type, idf_method='smooth')
-    assert vectorizer.fit_transform([text]).sum() == 0
+
+    with tokenizer_context("icu") as Doc:
+        assert vectorizer.fit_transform([Doc(text)]).sum() == 0
