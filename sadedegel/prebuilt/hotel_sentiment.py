9161c1b7bc9b5e98a656dc03d9fb203825846524
onatyap <oyapici17@ku.edu.tr>
2021-04-17 21:37:13 +0300

    Implement hotel_sentiment prebuilt model based on hotel_sentiment dataset implementation

---
+++ sadedegel/prebuilt/hotel_sentiment.py
@@ -0,0 +1,85 @@
+from os.path import dirname
+from pathlib import Path
+
+from joblib import dump
+from rich.console import Console
+from sklearn.metrics import f1_score
+from sklearn.pipeline import Pipeline
+from sklearn.svm import SVC
+from sklearn.utils import shuffle
+
+from .util import load_model
+from ..dataset.hotel_sentiment import load_hotel_sentiment_train, load_hotel_sentiment_test, \
+    load_hotel_sentiment_test_label, CORPUS_SIZE
+from ..extension.sklearn import HashVectorizer, Text2Doc
+
+console = Console()
+
+
+def empty_model():
+    return Pipeline(
+        [('text2doc', Text2Doc("icu")),
+         ('hash', HashVectorizer(n_features=413833, alternate_sign=False)),
+         ('svc', SVC(C=0.28610731097622305, kernel="linear", verbose=True, random_state=42, probability=True))]
+    )
+
+
+def build(save=True):
+    try:
+        import pandas as pd
+    except ImportError:
+        console.log(("pandas package is not a general sadedegel dependency."
+                     " But we do have a dependency on building our prebuilt models"))
+
+    raw = load_hotel_sentiment_train()
+    df = pd.DataFrame.from_records(raw)
+    df = shuffle(df)
+
+    console.log(f"Corpus Size: {CORPUS_SIZE}")
+
+    pipeline = empty_model()
+
+    pipeline.fit(df.text, df.sentiment_class)
+
+    evaluate(pipeline)
+
+    console.log("Model build [green]DONE[/green]")
+
+    if save:
+        model_dir = Path(dirname(__file__)) / 'model'
+
+        model_dir.mkdir(parents=True, exist_ok=True)
+
+        pipeline.steps[0][1].Doc = None
+
+        dump(pipeline, (model_dir / 'hotel_sentiment_classification.joblib').absolute(), compress=('gzip', 9))
+
+        console.log("Model save [green]DONE[/green]")
+
+
+def load(model_name="hotel_sentiment_classification"):
+    return load_model(model_name)
+
+
+def evaluate(model=None):
+    try:
+        import pandas as pd
+    except ImportError:
+        console.log(("pandas package is not a general sadedegel dependency."
+                     " But we do have a dependency on building our prebuilt models"))
+
+    if model is None:
+        model = load()
+
+    test = pd.DataFrame.from_records(load_hotel_sentiment_test())
+    testLabel = pd.DataFrame.from_records(load_hotel_sentiment_test_label())
+
+    test = test.merge(testLabel, on='id')
+
+    y_pred = model.predict(test.text)
+
+    console.log(f"Model test accuracy (f1-macro): {f1_score(test.sentiment_class, y_pred, average='macro')}")
+
+
+if __name__ == "__main__":
+    build(save=True)
