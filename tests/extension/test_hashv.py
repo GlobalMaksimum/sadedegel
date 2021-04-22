2c44b7b35ca697d5cde03d4e634fd544c62a26a6
Husnu Sensoy <husnu.sensoy@globalmaksimum.com>
2021-04-23 00:40:47 +0300

    Convert prefix_range to tuple rather than list (similar to sklearn ngram_range parameter)   Also ensure that you don't use pandas as any kind of sadedegel dependency (including tests) because it is optional

--- tests/extension/test_hashv.py
+++ tests/extension/test_hashv.py
@@ -1,10 +1,11 @@
-from sklearn.pipeline import Pipeline
+from itertools import islice, product
+
 import pytest
-from itertools import product
-import pandas as pd
+from sklearn.pipeline import Pipeline
+
 from .context import HashVectorizer, Text2Doc, load_tweet_sentiment_train
 
-p = product(["icu", "simple", "bert"], [(1, 3), [2, 5], (3, 8), [3, 6]], [True, False])
+p = product(["icu", "simple", "bert"], [(1, 3), (2, 5), (3, 8), (3, 6)], [True, False])
 
 
 @pytest.mark.parametrize("tokenizer, prefix_range, alternate_sign", p)
@@ -12,7 +13,11 @@ def test_hashvect(tokenizer, prefix_range, alternate_sign):
     feng_pipeline = Pipeline([("Text2Doc", Text2Doc(tokenizer=tokenizer)),
                               ("HashVec", HashVectorizer(prefix_range=prefix_range,
                                                          alternate_sign=alternate_sign))])
+    X = [rec['tweet'] for rec in islice(load_tweet_sentiment_train(), 10)]
+    assert feng_pipeline.fit_transform(X).shape[0] == len(X)
 
-    df = pd.DataFrame().from_records(load_tweet_sentiment_train()).sample(10, random_state=42)
 
-    assert feng_pipeline.fit_transform(df["tweet"]).shape[0] == len(df)
+def test_prefix_range_type_error():
+    with pytest.raises(ValueError, match=r"prefix_range should be of tuple type.*"):
+        _ = Pipeline([("Text2Doc", Text2Doc(tokenizer="icu")),
+                      ("HashVec", HashVectorizer(prefix_range=[1, 3]))])
