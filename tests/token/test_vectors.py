c82d0ae0dcb6c11bcef20488f7fd3e7c1fa4ec07
dafajon <dorukhan.afacan@globalmaksimum.com>
2021-11-22 16:20:54 +0300

    Skip test wrt to torch dependency

--- tests/token/test_vectors.py
+++ tests/token/test_vectors.py
@@ -1,6 +1,7 @@
-from .context import Doc, Vocabulary, WordVectorNotFound, tokenizer_context
+import pkgutil  # noqa: F401 # pylint: disable=unused-import
 import pytest
 
+from .context import Doc, Vocabulary, WordVectorNotFound, tokenizer_context
 
 common_phrase = "Merhaba dünya. Biz dostuz. Barış için geldik."
 
@@ -18,10 +19,23 @@ def test_indices(tokenizer):
         assert voc.id_to_feature_has_vector[voc.id_has_vector("dünya")] == "dünya"
 
 
-@pytest.mark.parametrize("tokenizer", ["bert", "simple", "icu"])
+@pytest.mark.parametrize("tokenizer", ["simple", "icu"])
 def test_vector(tokenizer):
     voc = Vocabulary(tokenizer)
     with tokenizer_context(tokenizer) as Doc2:
+        d = Doc2(common_phrase)
+        if voc.has_vector("merhaba"):
+            assert d.tokens[0].vector.shape[0] == 100
+        else:
+            with pytest.raises(WordVectorNotFound, match=".*Word Vector not found for.*"):
+                vector = d.tokens[0].vector.shape[0] == 100
+                assert vector is None
+
+
+@pytest.mark.skipif('pkgutil.find_loader("torch") is None')
+def test_vector_bert():
+    voc = Vocabulary("bert")
+    with tokenizer_context("bert") as Doc2:
         d = Doc2(common_phrase)
         if voc.has_vector("merhaba"):
             assert d.tokens[0].vector.shape[0] == 100
