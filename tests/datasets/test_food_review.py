32731543ffc7eb4c71fe5d691a231fcf8d6e18b7
dafajon <dorukhan.afacan@globalmaksimum.com>
2021-12-07 18:20:19 +0300

    Fix pandas dependency on test cases of food review sentiment

--- tests/datasets/test_food_review.py
+++ tests/datasets/test_food_review.py
@@ -1,9 +1,19 @@
 from pathlib import Path  # pylint: disable=unused-import
 from os.path import expanduser  # pylint: disable=unused-import
 
-import pandas as pd
 import pytest
 from .context import load_food_review_train, load_food_review_test, FOOD_CLASS_VALUES
+
+
+@pytest.mark.skipif('not Path(expanduser("~/.sadedegel_data/food_review")).exists()')
+def test_from_records():
+    raw_train = load_food_review_train()
+    raw_test = load_food_review_test()
+
+    for record in raw_train:
+        assert {"id", "text", "sentiment_class", "speed", "service", "flavour"} == set(list(record.keys()))
+    for record in raw_test:
+        assert {"id", "text", "sentiment_class", "speed", "service", "flavour"} == set(list(record.keys()))
 
 
 @pytest.mark.skipif('not Path(expanduser("~/.sadedegel_data/food_review")).exists()')
@@ -30,15 +40,3 @@ def test_dataset_import():
 def test_sentiment_class():
     assert FOOD_CLASS_VALUES[0] == "NEGATIVE"
     assert FOOD_CLASS_VALUES[1] == "POSITIVE"
-
-
-@pytest.mark.skipif('not Path(expanduser("~/.sadedegel_data/food_review")).exists()')
-def test_from_records():
-    raw_train = load_food_review_train()
-    raw_test = load_food_review_test()
-
-    train_df = pd.DataFrame().from_records(raw_train)
-    test_df = pd.DataFrame().from_records(raw_test)
-
-    assert set(train_df.columns.tolist()) == {"id", "text", "sentiment_class", "speed", "service", "flavour"}
-    assert set(test_df.columns.tolist()) == {"id", "text", "sentiment_class", "speed", "service", "flavour"}
