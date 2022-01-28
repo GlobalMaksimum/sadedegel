a5b67ad58cfc9ace4b2461b7dd249e1e786e2e70
dafajon <dorukhan.afacan@globalmaksimum.com>
2022-01-28 15:06:28 +0300

    Update supervised summarizer implemantation. Remove builder pattern. Implement class variable for lazy loading of model.

--- sadedegel/summarize/supervised.py
+++ sadedegel/summarize/supervised.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import numpy as np
 from typing import List
 import joblib
+from rich.console import Console
 
 from ._base import ExtractiveSummarizer
 from ..bblock.util import __transformer_model_mapper__
@@ -13,23 +14,43 @@ from ..config import load_config
 
 
 __vector_types__ = list(__transformer_model_mapper__.keys()) + ["tfidf", "bm25"]
+console = Console()
 
 
 def load_model(vector_type):
     name = f"ranker_{vector_type}.joblib"
     path = (Path(dirname(__file__)) / 'model' / name).absolute()
-    return joblib.load(path)
+    console.log(f"Initializing ranker model ranker_{vector_type}...", style="blue")
+
+    try:
+        model = joblib.load(path)
+    except Exception as e:
+        raise FileNotFoundError(f"A model trained for {vector_type} is not found. Please optimize one with "
+                                f"sadedegel.summarize.RankerOptimizer.")
+    return model
 
 
 class SupervisedSentenceRanker(ExtractiveSummarizer):
-    def __init__(self, builder, normalize):
+    model = None
+    vector_type = None
+    tags = ExtractiveSummarizer.tags + ["ml", "supervised", "rank"]
+
+    def __init__(self, normalize=True, vector_type="bert_128k_cased"):
         super().__init__(normalize)
-        self.model = builder.model
-        self.vector_type = builder.vector_type
+        self.init_model(vector_type)
+
+    @classmethod
+    def init_model(cls, vector_type):
+        if vector_type not in __vector_types__:
+            raise ValueError(f"Not a valid vectorization for input sequence. Valid types are {__vector_types__}")
+        if cls.vector_type is not None:
+            if cls.vector_type == vector_type:
+                return 0
+
+        cls.model = load_model(vector_type)
+        cls.vector_type = vector_type
 
     def _predict(self, sents: List[Sentences]) -> np.ndarray:
-        if self.vector_type not in __vector_types__:
-            raise ValueError(f"Not a valid vectorization for input sequence. Valid types are {__vector_types__}")
         if self.vector_type not in ["tfidf", "bm25"]:
             doc_sent_embeddings = self._get_pretrained_embeddings(sents)
         else:
@@ -53,18 +74,14 @@ class SupervisedSentenceRanker(ExtractiveSummarizer):
         pass
 
 
-class SupervisedSummarizer:
-    def __init__(self, vector_type=None, **kwargs):
-        if vector_type is None:  # Override config if user input is provided.
-            self.vector_type = load_config(kwargs)["summarizer"]["vector_type"]
-        else:
-            self.vector_type = vector_type
-        self.model = load_model(self.vector_type)
+class RankerOptimizer:
+    def __init__(self, vector_type: str, summarization_perc: float,**kwargs):
+        self.vector_type = vector_type
+        self.summarization_perc = summarization_perc
 
-    def __call__(self):
-        return SupervisedSentenceRanker(builder=self, normalize=True)
 
-    def optimize(self, summarization_perc: float):
+    @property
+    def optimize(self):
         """Optimize the ranker model for a custom summarization percentage. Optimize and dump a new model.
 
         Parameters
@@ -72,4 +89,7 @@ class SupervisedSummarizer:
         summarization_perc: float
             Percentage of summary to optimize for. Range: [0, 1]
         """
+        pass
+
+    def _prepare_dataset(self):
         pass
